2e30445ab8d051859414df154cb8fd378e260215
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-04-07 19:49:09 -0700

    basic node app done

---
+++ emoticon.js
@@ -0,0 +1,20 @@
+exports.shrug = function() {
+  console.log(`
+  ¯\\_(ツ)_/¯
+  `);
+}
+
+exports.bunny = function() {
+  console.log(
+  `
+  (\\_/)
+  (o.o)
+  (")(")
+  `);
+};
+
+exports.tableFlip = function() {
+  console.log(`
+  (╯°□°）╯︵ ┻━┻
+  `);
+};
---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require('./greeting.js');
+const emoticons = require('./emoticon.js');
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+// (\_/)
+// (o.o)
+// (")(")
+emoticons.bunny();
+
+// (╯°□°）╯︵ ┻━┻
+emoticons.tableFlip();
+
+// ¯\_(ツ)_/¯
+emoticons.shrug();
+
